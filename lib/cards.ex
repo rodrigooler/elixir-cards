6fb11640970ed2d208d1d830223fbcde163cbb89
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2018-01-24 01:00:24 -0200

    Create function shuffle

--- lib/cards.ex
+++ lib/cards.ex
@@ -2,6 +2,11 @@ defmodule Cards do
   def create_deck do
     ["Ace", "Two", "Three"]
   end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
   def sort_deck do
     Enum.sort(create_deck())
   end
