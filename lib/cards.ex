15750ca1a6ccabb9e5c2239b4c553bad1fbbbc5b
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2018-01-24 00:02:12 -0200

    Create function create_deck

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,18 +1,5 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for Cards.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def create_deck do
+    ["Ace", "Two", "Three"]
   end
 end
